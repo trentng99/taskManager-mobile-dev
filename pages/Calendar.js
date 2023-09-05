a21873b2fe47e4390711e253f3599c047aeb7291
trentng99 <trentngqx@gmail.com>
2023-09-06 00:02:32 +0800

    Scrollable calendar and shows tasks for the month

--- pages/Calendar.js
+++ pages/Calendar.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, TouchableOpacity, StatusBar } from "react-native";
 import { Text, Card } from "react-native-paper";
 import { globalStyles } from "../commons/styles";
 import { Agenda } from "react-native-calendars";
-
-let id = 0;
+import { formatDate } from "../commons/formatDate";
 
 export default function Calendar({ todos, setTodos }) {
   const [filteredTasks, setFilteredTasks] = useState({});
+  const [selectedDate, setSelectedDate] = useState(formatDate(new Date())); // Default to the current date
 
   const filterTasksByDate = (selectedDate) => {
     return todos.filter((task) => {
@@ -19,18 +19,38 @@ export default function Calendar({ todos, setTodos }) {
     });
   };
 
+  const loadItemsForTheMonth = (month) => {
+    const startDate = new Date(month.dateString);
+    startDate.setDate(1); // Set the start date to the 1st day of the mth
+    const endDate = new Date(month.dateString);
+    endDate.setMonth(endDate.getMonth() + 1);
+    endDate.setDate(0); // Set the end date to the last day of the mth
+
+    const items = {};
+
+    // Loop through each day in the month and get the tasks for that day
+    while (startDate <= endDate) {
+      const dateString = formatDate(startDate.toDateString());
+      const filteredTasksForDate = filterTasksByDate(dateString);
+
+      // Adding the filteredTasks to the items object using the date as the key
+      items[dateString] = filteredTasksForDate;
+
+      startDate.setDate(startDate.getDate() + 1); // Move to the next day
+    }
+
+    setFilteredTasks(items);
+  };
+
+  useEffect(() => {
+    // Load items for the current month when the page first renders
+    loadItemsForTheMonth({ dateString: selectedDate });
+  }, []);
+
   const onDayPress = (day) => {
     const selectedDate = day.dateString;
-    const filteredTasksForDate = filterTasksByDate(selectedDate);
-
-    // Debugging: Log the selected date and filtered tasks
-    console.log("Selected Date:", selectedDate);
-    console.log("Filtered Tasks:", filteredTasksForDate);
-
-    // Update the state with the filtered tasks using the selected date as the key
-    setFilteredTasks({
-      [selectedDate]: filteredTasksForDate,
-    });
+    setSelectedDate(selectedDate);
+    loadItemsForTheMonth(day);
   };
 
   const renderItem = (item) => {
@@ -40,6 +60,7 @@ export default function Calendar({ todos, setTodos }) {
           <Card.Content>
             <View>
               <Text>{item.value}</Text>
+              <Text>{item.endDate}</Text>
             </View>
           </Card.Content>
         </Card>
@@ -55,11 +76,13 @@ export default function Calendar({ todos, setTodos }) {
       <Agenda
         onDayPress={onDayPress}
         items={filteredTasks}
+        selected={selectedDate}
         renderItem={renderItem}
         refreshControl={null}
         showClosingKnob={true}
         refreshing={false}
       />
+      <StatusBar />
     </View>
   );
 }
